f143e69d935574b08a0fe4becbe0f8e5caceba9c
camicastro <camicastro.11.94@gmail.com>
2023-09-14 15:08:20 -0300

    Subiendo el frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+import Home from "./components/Home/Home";
+import Detail from "./components/Detail/Detail";
+import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/pokemons" element={<Home />} />
+        <Route path="/pokemons/detail/:id" element={<Detail />} />
+        <Route path="/pokemons/create" element={<Form />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
