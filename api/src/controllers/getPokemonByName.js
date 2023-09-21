418e680073c3866b2570211dfa7df130a0e81bf0
camicastro <camicastro.11.94@gmail.com>
2023-09-21 16:01:51 -0300

    cambie T de types

--- api/src/controllers/getPokemonByName.js
+++ api/src/controllers/getPokemonByName.js
@@ -12,19 +12,19 @@ const getPokemonApiByName = async (name) => {
 
             let pokemon = pokemonsApiName; //guarda la informacion recibida en la variable pokemon
 
-            return { //retorna un objeto creado con los datos del pokemon encontrado en la api
+            return [{ //retorna un objeto creado con los datos del pokemon encontrado en la api
                 id: pokemon.data.id,
                 name: pokemon.data.name,
                 image: pokemon.data.sprites.other.dream_world.front_default,
                 //types: pokemon.data.types.map((pokemon) => pokemon.type.name), //devuelve un array con los tipos de p
-                types: pokemon.data.types.map((pokemon) => { return {name: pokemon.type.name}}),
+                Types: pokemon.data.types.map((pokemon) => { return {name: pokemon.type.name}}),
                 hp: pokemon.data.stats[0].base_stat,
                 attack: pokemon.data.stats[1].base_stat,
                 defense: pokemon.data.stats[2].base_stat,
                 speed: pokemon.data.stats[3].base_stat,
                 height: pokemon.data.height,
                 weight: pokemon.data.weight,
-            }; 
+            }]; 
 
         }else {
             return null;
@@ -40,7 +40,6 @@ const getPokemonDbByName = async(name) => {
         const pokemonDbName = await Pokemon.findAll({ //busco en el modelo Pokemon por nombre
             where: {
                 name: name.toLowerCase(), // insensible a mayusculas y minusculas.
-                
               },
               include: {
                 attributes: ["name"], //incluye informacion del modelo Type.
